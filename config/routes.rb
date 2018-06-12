65515136843e1a4895ad67a4ced43647154dcf11
Steven Ng <stevenrng@gmail.com>
2018-06-12 14:54:01 -0400

    first commit set up with devise

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => {:registrations => "registrations"}
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+  root 'schedule#index'
+  resources 'schedule'
+end
