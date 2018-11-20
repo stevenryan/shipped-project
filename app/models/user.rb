2d63d28cec58d6b2cc377e927690ed987a1b0423
Steven Ng <stevenrng@gmail.com>
2018-11-20 15:58:55 -0500

    jobs and boats dependent on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :boats
-  has_many :jobs
+  has_many :boats, dependent: :destroy
+  has_many :jobs, dependent: :destroy
 end
